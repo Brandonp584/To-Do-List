7aac3c3d9f6e066829e43cb8472340eac5b495c0
Brandon Powell <Brandon.powell98@outlook.com>
2026-05-02 18:04:53 +1000

    feat: complete backend authentication with login and jwt

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -40,4 +40,50 @@ router.post("/register", async (req, res) => {
     }
 });
 
+// LOGIN
+router.post("/login", async (req, res) => {
+    try {
+        const {email, password} = req.body;
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({
+                message: "Invalid credentials"
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({
+                message: "Invalid credentials"
+            });
+        }
+
+        const jwt = require("jsonwebtoken");
+
+        const token = jwt.sign(
+            { id: user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: "1h" }
+        );
+
+        res.json({
+            message: "Login Successful",
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email
+            }
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+});
+
 module.exports = router;
