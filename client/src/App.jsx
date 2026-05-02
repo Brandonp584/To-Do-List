b66f7d433d6084dfa0eb9bf44d71fe9763dacf51
Brandon Powell <Brandon.powell98@outlook.com>
2026-05-02 15:42:32 +1000

    feat: add task creation from frontend UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,46 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [tasks, setTasks] = useState([]);
+  const [title, setTitle] = useState("");
 
-  useEffect(() => {
+  // Get Tasks
+  const fetchTasks = () => {
     fetch("http://localhost:5000/api/tasks")
       .then(res => res.json())
       .then(data => setTasks(data));
+  };
+
+  useEffect(() => {
+    fetchTasks();
   }, []);
+
+  //CREATE task
+  const addTask = async () => {
+    if (!title) return;
+
+    await fetch("http://localhost:5000/api/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ title })
+    });
+
+    setTitle("");
+    fetchTasks();
+  }
 
   return (
     <div style={{ padding: "20px" }}>
       <h1>To-Do-List</h1>
+
+      <input
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="Enter Task"
+      />
+
+      <button onClick={addTask}>Add Task</button>
 
       {tasks.map(task => (
         <div key={task._id}>
